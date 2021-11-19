af45cf8130eaaf3e11f9b7151dae73e915e62b04
Sebastian Rettig <serettig@posteo.de>
2021-11-19 12:02:03 +0100

    fix: updated JS scripts and removed unneeded ones

--- public/js/main.js
+++ public/js/main.js
@@ -349,50 +349,7 @@
 	};
 	OnePageNav();
 
-
-	// magnific popup
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
-    }
-  });
-
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-  });
-
-   
-   $('#checkin_date, #checkout_date, #birth_date, #arrival_date, #departure_date, #expiration_date').datepicker({
-	  'format': 'm/d/yyyy',
-	  'autoclose': true
-	});
-	$('#book_time, #arrival_time, #departure_time').timepicker();
-
-
-
-
-
-	// TYPEWRITE
+  // TYPEWRITE
 	var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
---
+++ scripts/copy-js.sh
@@ -0,0 +1,4 @@
+cp node_modules/jquery/dist/jquery.min.js ./public/js
+cp node_modules/jquery-migrate/dist/jquery-migrate.min.js ./public/js
+cp node_modules/bootstrap/dist/js/bootstrap.min.js ./public/js
+cp node_modules/popper.js/dist/umd/popper.min.js ./public/js
